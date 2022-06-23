71fa1433b05de359de70870185003d23fe9ca41a
Murat <laserdancer06@gmail.com>
2022-06-23 11:58:19 +0300

    End of day 19's commit in ISTANBUL and special thanks to GURHAN :)

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,15 +6,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {
+        plugin = { //the name of the report file that report type and  where to store
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
-        },                                          //report type and  where to store
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"//"PrettyReports" prints out additional information about the scenario
+                //this plugin generates the "MAVEN CUCUMBER REPORTING
+        },
         features = "src/test/resources/features",   //path of the features directory
         glue = "com/cydeo/step_definitions",   //path to step_def
         dryRun = false,   //execute step_def or not. we can change it true to get snippets only and then again we change it false!
-        tags = "@wip"   //it control to which scenario is run
+        tags = "@wip", //it control to which scenario is run @work in progress
+        publish = true
+
 )
 
 public class CukesRunner {
